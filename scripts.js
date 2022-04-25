1baf3fbf42907304845ed296cc839571b6ff55c9
castamerrel <103581279+castamerrel@users.noreply.github.com>
2022-04-25 20:28:36 +0400

    fin.deploy

--- scripts.js
+++ scripts.js
@@ -1,9 +1,16 @@
 const tabs = document.querySelectorAll('.tab')
 const tabContents = document.querySelectorAll('.tabcontent')
+const cryptoTexts = document.querySelectorAll('.inputText')
+const cryptoBtns = document.querySelectorAll('.copyText')
+const cryptoAlert = document.querySelector('.contacts_crypto')
 
 
 tabs.forEach(function(el) {
     el.addEventListener('click', activateTab);
+});
+
+cryptoBtns.forEach(function(el) {
+    el.addEventListener('click', copyCrypto);
 });
 
 function activateTab(el) {
@@ -22,4 +29,29 @@ function activateTab(el) {
         element.classList.remove('visually-hidden');
         element.classList.add("active");
     }
+}
+
+function copyCrypto(el) {
+    const id = el.target.dataset.id;
+
+    cryptoTexts.forEach(cryptoText => {
+        if (cryptoText.id == id) {
+
+            cryptoText.select();
+            document.execCommand('copy');
+            showAlert();
+            cryptoTexts.forEach(cryptoText => {
+                cryptoText.classList.remove('active');
+            });
+            cryptoText.classList.add('active');
+        };
+    });
+}
+
+function showAlert() {
+    cryptoAlert.classList.add("active");
+    window.getSelection().removeAllRanges();
+    setTimeout(function(){
+        cryptoAlert.classList.remove("active");
+    }, 700);
 }
